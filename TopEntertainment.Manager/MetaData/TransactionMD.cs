5101021077848ce1a6f137826f498c99aec88013
mike.chen <tmal0909@gmail.com>
2019-11-07 13:43:20 +0800

    [Manager] 更新交易欄位

--- TopEntertainment.Manager/MetaData/TransactionMD.cs
+++ TopEntertainment.Manager/MetaData/TransactionMD.cs
@@ -12,10 +12,13 @@ namespace TopEntertainment.Manager.MetaData
         [HiddenInput]
         public int MemberId { get; set; }
 
+        [Display(Name = "會員名稱", Prompt = "請輸入會員名稱")]
         public string MemberName { get; set; }
 
+        [Display(Name = "會員餘額", Prompt = "請輸入會員餘額")]
         public decimal Integration { get; set; }
 
+        [Display(Name = "交易積分", Prompt = "請輸入交易積分")]
         public decimal DealIntegration { get; set; }
     }
 }
